038ed7dcebfb819f586a03ce56bd81c913c6635d
Oskari Virtanen <oskari@flowdock.com>
2015-08-11 20:11:47 +0300

    Add tests

---
+++ tests.js
@@ -0,0 +1,13 @@
+var expect = require("expect.js");
+var mangle = require("./index.js");
+
+describe("mangle", function() {
+  it("can mangle a word", function() {
+    expect(mangle("foobar")).to.eql("______");
+  });
+
+  it("can mangle a sentence, leaving spaces alone", function() {
+    expect(mangle("The quick brown fox jumps over the lazy dog"))
+      .to.eql("___ _____ _____ ___ _____ ____ ___ ____ ___");
+  });
+});
